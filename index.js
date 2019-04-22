114ea0d664188dcf96d9e31ba04eea986a063e48
Prashant <bhandariprashant616@gmail.com>
2019-04-21 22:53:00 -0500

    Added javascript code to fetch api data

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
-$('#state-selection-form').submit(function(){
+$(function(){
+    $('#num-results').val("10");
+});
+
+
+$('#state-selection-form').submit(function(evt){
+    evt.preventDefault();
     let ddSelect = [];
         ddSelect = $('.state-select').val();
-        let maxCnt = $('#frm-MaxCount').val();
-        CallAPi(maxCnt,ddSelect);
+        let maxCnt = $('#num-results').val();
+        CallAPi(maxCnt,ddSelect.toString());
     });
     
     // a function to call the API with the query parameters
         // promise
     
-    function CallAPi(maxCount,selectedStates)
-    {
-    
+function CallAPi(maxCount, selectedStates) {
+    let url = 'https://developer.nps.gov/api/v1/parks';
+    let queryParam = {
+     stateCode:selectedStates,
+     limit:Number(maxCount) - 1,
+     fields:"Addresses",
+     api_key :'9EZ1RXLQdWm5d8nymt3rXc6PTiyPUAvT5Ft60NgR'
     }
+    let queryString = BuildQueryString(queryParam);
+    url = url + '?' + queryString;
+    fetch(url)
+    .then(response=>response.json())
+    .then(jsonData=>DisplayResult(jsonData))
+    .catch(err=>{
+        $('#finalResult').html(`Error Occurred : ${err.message}`);  
+    });
     
+}
+
+function BuildQueryString(parameters)
+ {
+     //Build a query string from the parameters data
+     let keys = Object.keys(parameters);
+    let queryItems= keys.map(k=>`${k}=${parameters[k]}`);
+     return queryItems.join('&');
+ } 
+ 
+ function DisplayResult(jsonResult)
+ {
+     console.log(jsonResult.data.length);
+     let dataAppend = 
+    $('.finalResult').html('')
+     let finalResult = `<table id="tblResult">
+     <tr>
+     <th>Full Name </th>
+     <th>Description </th>
+     <th>Wesite URL</th>
+     <th>Addresses</th>
+     </tr>
+     </table>`;
+     $('.finalResult').html(finalResult);
+     
+     for(let i = 0 ; i< jsonResult.data.length ; i++)
+     {
+        $('#tblResult').append(`<tr><td>${jsonResult.data[i].fullName}</td>
+        <td>${jsonResult.data[i].description}</td>
+        <td>${jsonResult.data[i].url}</td>
+        </tr>`);
+     }
+    
+  
+     
+     
+ }
     
     
     
